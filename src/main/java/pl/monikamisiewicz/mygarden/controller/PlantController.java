995566619a4f42f106017dd3a96096817c56cc9e
Monika Misiewicz <zieba.monika@wp.pl>
2021-01-04 15:01:01 +0100

    added 3 methods to PlantController and add-plant file

--- src/main/java/pl/monikamisiewicz/mygarden/controller/PlantController.java
+++ src/main/java/pl/monikamisiewicz/mygarden/controller/PlantController.java
@@ -1,4 +1,41 @@
 package pl.monikamisiewicz.mygarden.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import pl.monikamisiewicz.mygarden.model.Category;
+import pl.monikamisiewicz.mygarden.model.Plant;
+import pl.monikamisiewicz.mygarden.model.Status;
+import pl.monikamisiewicz.mygarden.service.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
 public class PlantController {
+
+    private final PlantService plantService;
+    private final CategoryService categoryService;
+    private final ExposureService exposureService;
+    private final BloomTimeService bloomTimeService;
+    private final StatusService statusService;
+
+    @GetMapping("/add-plant")
+    public String addPlant(Model model) {
+        model.addAttribute("plant", new Plant());
+        return "add-plant";
+    }
+
+    @ModelAttribute("cateogories")
+    public List<Category> categories() {
+        return categoryService.getCategories();
+    }
+
+    @ModelAttribute("statuses")
+    public List<Status> statuses() {
+        return statusService.getStatuses();
+    }
+
 }
